5d035e6acf002539acc46670adf9a53388b3697f
Carlos <cuzikjack@gmail.com>
2019-08-28 16:59:10 -0300

    asd

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    semi: [2, 'never'],
   },
   parserOptions: {
     parser: 'babel-eslint',
   },
-};
+}
